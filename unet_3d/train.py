d9f4a5648435770051a0bcb2cf43a20c8b51caa0
Daniel Hernandez <dani.hp85@gmail.com>
2019-07-02 15:34:56 +0200

    3d unet using new data.

--- unet_3d/train.py
+++ unet_3d/train.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 
-# IMPORTING MODULES
+
 import numpy as np
 from netCDF4 import MFDataset
 from sklearn.model_selection import train_test_split
 
 from unet_3d import model
 
-dataIn = 'CloudCover6hourly2000_nc/'
+ncfile_r = MFDataset('ShortTrain_1year/clt/*.nc')
 
-# LOADING DATA
-ncfile_r = MFDataset(dataIn + '*.nc')
 maps = ncfile_r.variables['clt']
 
 Nt, Ny, Nx = maps.shape
 print('{} maps ready to be loaded'.format(Nt))
+
+x0 = 0
+xn = 128
+y0 = 0
+yn = 128
 
 
 # DEFINING BATCH DATA GENERATOR
@@ -79,7 +82,7 @@ def train_split_seq(maps, Nframes, r_train=0.7, r_val=0.2, r_test=0.1, random_st
     return it_train, it_val, it_test
 
 
-def batch_generator(maps, it_train, Nframes, batch_size=32):
+def batch_generator(maps, x0, xn, y0, yn, it_train, Nframes, batch_size=32):
     # Batch data generator.
     # INPUTS:
     # - maps: netcdf object representing all maps from the set of
@@ -113,12 +116,12 @@ def batch_generator(maps, it_train, Nframes, batch_size=32):
         for it_batch in range(0, Lt_train - dit_batch, dit_batch):
             it_batch_train = it_train[np.arange(it_batch, it_batch + dit_batch)]
 
-            XYtrain = maps[it_batch_train, :, :]
+            XYtrain = maps[it_batch_train, x0:xn, y0:yn]
 
-            Xtrain_i = XYtrain[iX, :, :].reshape(batch_size, Nframes, Ny, Nx)
+            Xtrain_i = XYtrain[iX, :, :].reshape(batch_size, Nframes, xn, yn)
             Xtrain_i = np.squeeze(Xtrain_i)
 
-            Ytrain_i = XYtrain[iY, :, :].reshape(batch_size, Ny, Nx)
+            Ytrain_i = XYtrain[iY, :, :].reshape(batch_size, xn, yn)
 
             Xtrain_i = np.expand_dims(Xtrain_i, axis=Xtrain_i.ndim)
             Ytrain_i = np.expand_dims(Ytrain_i, axis=Ytrain_i.ndim)
@@ -129,10 +132,11 @@ def batch_generator(maps, it_train, Nframes, batch_size=32):
             yield (Xtrain_i, Ytrain_i)
 
 
-def validation_generator(maps, it_val, Nframes):
+def validation_generator(maps, x0, xn, y0, yn, it_val, Nframes, batch_size=32):
     Nframes_with_test = Nframes + 1
 
-    XYval = maps[it_val, :, :]
+    XYval = maps[it_val, x0:xn, y0:yn]
+    print(XYval.shape)
     Nt_val, Ny, Nx = XYval.shape
 
     Nseq_val = np.int(Nt_val / Nframes_with_test)
@@ -143,8 +147,10 @@ def validation_generator(maps, it_val, Nframes):
 
     Xval = XYval[iX, :, :].reshape(Nseq_val, Nframes, Ny, Nx)
     Xval = np.squeeze(Xval)
+    print(XYval[iY, :, :].shape)
 
-    Yval = XYval[iY, :, :]
+    Yval = XYval[iY, :, :]  # .reshape(batch_size, Ny, Nx)
+    print(Yval.shape)
 
     Xval = np.expand_dims(Xval, axis=Xval.ndim)
     Yval = np.expand_dims(Yval, axis=Yval.ndim)
@@ -154,32 +160,6 @@ def validation_generator(maps, it_val, Nframes):
     print(Xval.shape)
 
     return (Xval, Yval)
-
-
-def test_generator(maps, it_test, Nframes):
-    Nframes_with_test = Nframes + 1
-
-    XYtest = maps[it_test, :, :]
-    Nt_test, Ny, Nx = XYtest.shape
-
-    Nseq_test = np.int(Nt_test / Nframes_with_test)
-
-    iXY = range(Nt_test)
-    iY = range(Nframes, Nt_test, Nframes_with_test)
-    iX = ~np.in1d(iXY, iY)
-
-    Xtest = XYtest[iX, :, :].reshape(Nseq_test, Nframes, Ny, Nx)
-    Xtest = np.squeeze(Xtest)
-
-    Ytest = XYtest[iY, :, :]
-
-    Xtest = np.expand_dims(Xtest, axis=Xtest.ndim)
-    Ytest = np.expand_dims(Ytest, axis=Ytest.ndim)
-    Ytest = np.expand_dims(Ytest, axis=Ytest.ndim)
-
-    Xtest = np.transpose(Xtest, (0, 2, 3, 1, 4))
-
-    return (Xtest, Ytest)
 
 
 # BATCH GENERATOR DEFINITION
@@ -206,10 +186,10 @@ if r_train + r_val + r_test != 1:
 it_train, it_val, it_test = train_split_seq(maps, Nframes=Nframes, r_train=r_train, r_val=r_val, r_test=r_test, random_state=None)
 
 # Creating the batch generator to be used during training
-BG = batch_generator(maps, it_train, Nframes=Nframes, batch_size=batch_size)
+BG = batch_generator(maps, x0, xn, y0, yn, it_train, Nframes=Nframes, batch_size=batch_size)
 
 # Creating the validation data to be used during training
-VG = validation_generator(maps, it_val, Nframes)
+VG = validation_generator(maps, x0, xn, y0, yn, it_val, Nframes, batch_size=batch_size)
 
 print('{} maps in total'.format(Nt))
 print('{} / {} / {} maps for training / validation / test'.format(len(it_train), len(it_val), len(it_test)))
@@ -224,14 +204,13 @@ print('{} batches per epoch'.format(spe))
 print('{} epochs'.format(epochs))
 
 # Initializing the CNN
-model = model.get_unet_3d_2((256, 512, Nframes, 1))
+model = model.get_unet_3d((128, 128, Nframes, 1))
 
 # compiling the ANN
-# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['mean_squared_error'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['mean_squared_error'])
 # model.compile(optimizer='rmsprop',loss='binary_crossentropy', metrics = ['mean_squared_error'])
-model.compile(optimizer='adam',loss='mean_squared_error', metrics = ['mean_squared_error'])
+# model.compile(optimizer='adam',loss='mean_squared_error', metrics = ['mean_squared_error'])
 # model.compile(optimizer='rmsprop',loss='mean_squared_error', metrics = ['mean_squared_error'])
-
 
 model.summary()
 
